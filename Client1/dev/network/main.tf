6a43998afc241cbada3012ad94d74177b89a3848
Oscar Patricio <ext.oscar.patricio@farfetch.com>
2021-02-03 22:37:11 +0000

    feat(azure): add default NSGs

--- Client1/dev/network/main.tf
+++ Client1/dev/network/main.tf
@@ -10,6 +10,40 @@ module "rg" {
   custom_tags = var.custom_tags
 }
 
+module "nsg_pub" {
+  source              = "github.com/k0kazpt/_azure-terraform-module-nsg?ref=3.5.0"
+  resource_group_name = module.rg.name
+  security_group_name = join("-", [module.naming.network_security_group.name, "pub"])
+  tags                = var.tags
+  custom_rules        = var.custom_rules
+  predefined_rules = [
+    {
+      name     = "HTTP"
+      priority = 201
+    },
+    {
+      name     = "HTTPS"
+      priority = 202
+    }
+  ]
+
+  depends_on = [
+    module.rg
+  ]
+}
+
+module "nsg_prv" {
+  source              = "github.com/k0kazpt/_azure-terraform-module-nsg?ref=3.5.0"
+  resource_group_name = module.rg.name
+  security_group_name = join("-", [module.naming.network_security_group.name, "prv"])
+  tags                = var.tags
+  custom_rules        = var.custom_rules
+
+  depends_on = [
+    module.rg
+  ]
+}
+
 module "vnet" {
   source              = "github.com/k0kazpt/azure-terraform-module-vnet?ref=0.1.4"
   vnet_name           = module.naming.virtual_network.name
@@ -23,9 +57,15 @@ module "vnet" {
     "10.0.0.0/24",
     "10.0.1.0/24"
   ]
+  nsg_ids = {
+    pub = module.nsg_pub.network_security_group_id
+    prv = module.nsg_prv.network_security_group_id
+  }
+
   custom_tags = var.custom_tags
 
   depends_on = [
-    module.rg
+    module.nsg_pub,
+    module.nsg_prv
   ]
 }
--- Client1/dev/network/variables.tf
+++ Client1/dev/network/variables.tf
@@ -22,3 +22,30 @@ variable "custom_tags" {
     "customer" : "k0kazpt"
   }
 }
+
+variable "tags" {
+  description = "Default tags to be added"
+  type        = map(string)
+  default = {
+    "ManagedBy" : "Terraform"
+  }
+}
+
+variable "custom_rules" {
+  description = "Default rules to be applied to NSGs"
+  type        = any
+  default = [
+    {
+      name                       = "BlockFromInternet"
+      priority                   = 9000
+      direction                  = "Inbound"
+      access                     = "Deny"
+      protocol                   = "all"
+      source_address_prefix      = "Internet"
+      source_port_range          = "*"
+      destination_address_prefix = "*"
+      destination_port_range     = "*"
+      description                = "Block all inbound traffic from the Internet!"
+    }
+  ]
+}
