95a2508cd92a28daad36ef03dd7a72363971506d
Oscar Patricio <ext.oscar.patricio@farfetch.com>
2021-02-03 21:18:15 +0000

    fix(azure): fix cidr notation; update gitignore to ignore tfplan

--- Client1/dev/network/main.tf
+++ Client1/dev/network/main.tf
@@ -13,15 +13,15 @@ module "rg" {
 module "vnet" {
   source              = "github.com/k0kazpt/azure-terraform-module-vnet?ref=0.1.4"
   vnet_name           = module.naming.virtual_network.name
-  address_space       = ["10.0.1.0/22"]
+  address_space       = ["10.0.0.0/22"]
   resource_group_name = module.rg.name
   subnet_names = [
     "pub",
     "prv"
   ]
   subnet_prefixes = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
+    "10.0.0.0/24",
+    "10.0.1.0/24"
   ]
   custom_tags = var.custom_tags
 
